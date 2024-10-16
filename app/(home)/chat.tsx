20b8dbcec954a4d7164b16a80786a33cbd917301
Nmn <Naman.tortalabz@hotmail.com>
2024-10-16 16:44:54 +0530

    char service  added

--- app/(home)/chat.tsx
+++ app/(home)/chat.tsx
@@ -1,22 +1,35 @@
 import { View, Text, Image } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRoute } from "@react-navigation/native";
 import { chatStyles } from "@/styles/chatStyles";
 import ChatHeader from "@/components/chat/ChatHeader";
 import Chat from "@/components/chat/Chat";
 import SendButton from "@/components/chat/SendButton";
+import { usePaginatedChats } from "@/service/api/chatService";
+import { useChatStore } from "@/service/chatStore";
 
 const Page = () => {
   const route = useRoute() as any;
   const item = route?.params;
+  const {loadMoreChats, loading, hasMoreChats} = usePaginatedChats(item?.conversation_id);
   const [heightOfMessageBox, setHeightOfMessageBox] = useState(0);
   const [isTyping, setIsTyping] = useState(false);
+
+  const {conversations} = useChatStore.getState();
+  const currentChat = conversations.find(convo => convo.conversationId === item?.conversation_id);
+
+
+  useEffect(() => {
+    if(!loading && hasMoreChats){
+      loadMoreChats();
+    }
+  },[])
 
   return (
     <View style={chatStyles.container} >
       <ChatHeader item={item} />
       <Image source={require('@/assets/images/pattern.png')} style={chatStyles.background} />
-      <Chat heightOfMessageBox={heightOfMessageBox} messages={[]} onLoadMore={() => {}} loading={false} />
+      <Chat heightOfMessageBox={heightOfMessageBox} messages={ currentChat?.messages || []} onLoadMore={() => !loading && hasMoreChats && loadMoreChats()} loading={loading} />
       <SendButton item={item} isTyping={isTyping} setHeightOfMessageBox={setHeightOfMessageBox} setIsTyping={setIsTyping} />
     </View>
   );
--- service/api/chatService.tsx
+++ service/api/chatService.tsx
@@ -1,5 +1,6 @@
 import { appAxios } from "./apiInterceptors";
 import { useChatStore } from "../chatStore";
+import { useState } from "react";
 
 
 export const getAllConversations = async () => { 
@@ -11,4 +12,56 @@ export const getAllConversations = async () => {
     console.log('getAllConversations',error);
     return null;
   }
+}
+
+
+export const fetchPaginatedChats = async (conversationId: string, page:number=1) => { 
+  try {
+    const apiRes = await appAxios.get(`/chat/paginated-chats?conversationId=${conversationId}&page=${page}`);
+    return apiRes.data;
+  } catch (error) {
+    console.log('fetchPaginatedChats ERROR',error);
+    return null;
+  }
+}
+
+const updateChatStore = (conversationId: string, newMessages: any[]) => {
+  const {setConversations, conversations} = useChatStore.getState();
+  const updatedConversations = conversations.map((convo:any) => {
+      if(convo.conversationId === conversationId){
+
+        const existingMessageIds = new Set(convo.messages.map((message:any) => message.id));
+        const uniqueNewMessages = newMessages.filter((message:any) => !existingMessageIds.has(message.id));
+        const allMessages = [...uniqueNewMessages, ...convo.messages];
+        const sortedMessages = allMessages.sort((a:any, b:any) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+
+        return {...convo, messages: sortedMessages}
+      }
+      return convo;
+  })
+
+  setConversations(updatedConversations);
+  
+}
+
+export const usePaginatedChats = (conversationId: string) => {
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [hasMoreChats, setHasMoreChats] = useState(true);
+
+  const loadMoreChats = async () => {
+    if (loading || !hasMoreChats) return;
+    setLoading(true);
+    const data = await fetchPaginatedChats(conversationId, page);
+    if(data && data?.messages.length>0){
+      updateChatStore(conversationId, data.messages);
+      setPage(page+1);
+    }else{
+      setHasMoreChats(false);
+    }
+    setLoading(false);
+  }
+
+  return {loadMoreChats, loading, hasMoreChats};
+  
 }
